d1a28f713fe77d99e4ecd864dd771e08709a470e
Caeldeth <sabrael@hybrasyl.com>
2022-01-13 17:47:11 -0600

    create server

---
+++ server.js
@@ -0,0 +1,24 @@
+// import express
+const express = require('express');
+// import mongoose
+const mongoose = require('mongoose');
+// use express
+const app = express();
+
+// set up default server port
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// use routes
+app.use(require("./routes"));
+
+// useFindAndModify, useNewUrlParser, useUnifiedTopology are deprecated in mongoose v6, as they are set to false, true, true, respectively by default
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/whispr')
+
+// logs mongo queries
+mongoose.set('debug', true);
+
+// connect server
+app.listen(PORT, () => console.log(`Server listening to port: ${PORT}`));
