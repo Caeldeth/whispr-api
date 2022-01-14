6aea4bbbe57c7b0703f48aea4d8342973726e333
Caeldeth <sabrael@hybrasyl.com>
2022-01-13 20:41:18 -0600

    add reaction controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -54,16 +54,62 @@ const thoughtController = {
     },
 
     // update thought
-    updateThought({ params, body }, res) {},
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate (
+            { _id: params.id },
+            body,
+            { new: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'thoughtId not found' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
 
     // delete thought
-    deleteThought({ params }, res) {},
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+        .then((dbThoughtData) => {
+            if(!dbThoughtData) {
+                res.status(404).json({ message: 'thoughtId not found' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
 
     // add reaction
-    addReaction({ params, body }, res) {},
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { new: true}
+        )
+        .then((dbThoughtData) => {
+            if(!dbThoughtData) {
+                res.status(404).json({ message: "thoughtId not found" });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch((err) => res.json(err));
+    },
 
     // delete reaction
-    deleteReaction({ params, body}, res) {},
-}
+    deleteReaction({ params, body}, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: {reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err));
+    },
+};
 
 module.exports = thoughtController;
