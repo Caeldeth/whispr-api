b93a2f4774b77256b1e96472e90b6261f595ce86
Caeldeth <sabrael@hybrasyl.com>
2022-01-14 11:36:56 -0600

    move routes code to right file, create index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,8 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/user-controller");
-
-// set up all user routes
-router.route("/").get(getAllUsers).post(createUser);
-
-// set up one user routes
-router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
-
-// set up friend routes
-router.route("/:userId/friends/friendsId").post(addFriend).delete(removeFriend);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
