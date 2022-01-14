3444dd16c4f1c36ae4c0be4f49ef89aaa82b415f
Caeldeth <sabrael@hybrasyl.com>
2022-01-13 18:19:35 -0600

    create user model

--- models/User.js
+++ models/User.js
@@ -1,30 +1,37 @@
+const { Schema, model } = require("mongoose");
 
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/],
+        },
+        thoughts: [], //need to create thoughts model
+        friends: [], //need to create friends model
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
+// virtual to count how many friends a user has - use same virtual as pizza hunt
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
 
+// create the user model using userSchema
+const User = model('User', UserSchema)
 
-/*
-User
-
-username
-
-String
-Unique
-Required
-Trimmed
-email
-
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
-thoughts
-
-Array of _id values referencing the Thought model
-friends
-
-Array of _id values referencing the User model (self-reference)
-
-Schema Settings
-
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-*/
+// export the model
+module.exports = User;
